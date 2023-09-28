eff29d8fa2d17d782b3746e5636d48b46e47d34a
Nabil ML <n.oumellahni@gmail.com>
2023-09-28 21:49:26 +0100

    springboot rabbitmq

---
+++ src/main/java/com/springbootrabbitmq/controller/MessageController.java
@@ -0,0 +1,26 @@
+package com.springbootrabbitmq.controller;
+
+import com.springbootrabbitmq.publisher.RabbitMQProducer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MessageController {
+
+    private final RabbitMQProducer producer;
+
+    public MessageController(RabbitMQProducer producer) {
+        this.producer = producer;
+    }
+
+    @GetMapping("/publish")
+    public ResponseEntity<String> sendMessage(@RequestParam("message") String message) {
+        producer.sendMessage(message);
+        return new ResponseEntity<>("Message sent to RabbitMQ ...", HttpStatus.OK);
+    }
+}
